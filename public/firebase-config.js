bbd4f23d8405639e702d6013245cd3dbef85c49a
earlgeraldesparcia <earlgeraldesparcia@gmail.com>
2025-08-17 12:18:01 +0800

    Initial commit

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,21 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Replace with your Firebase project config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCwMiX7LUW_yIcriCvSkFWHog-aAO3Hssw",
+  authDomain: "smsitchat.firebaseapp.com",
+  databaseURL: "https://smsitchat-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "smsitchat",
+  storageBucket: "smsitchat.appspot.com",
+  messagingSenderId: "426807364618",
+  appId: "1:426807364618:web:751fdc0875bb7a4805a3f3",
+  measurementId: "G-XQGJ46SMH9"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
-
-// Optional: auto anonymous sign-in if needed
-signInAnonymously(auth).catch(err => console.error(err));
